2b7e22dd85e426a8bd794ad6a1d1ba2f1b292ca7
Stefano Notaro <stefano.notaro@visitor.upm.com>
2020-01-07 11:35:16 -0300

    Class 78

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TIMEOUT } from 'dns';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'spa';
+  nombre = 'Stefano';
+
+  arreglo = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  PI = Math.PI;
+
+  a = 0.234;
+
+  salario = 1234.5;
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'Primera',
+      casa: '19'
+    }
+  };
+
+  valorDePromesa = new Promise( ( resolve, reject ) => {
+    setTimeout(() => {
+      resolve( 'Llego la data!' );
+    }, 3500);
+  } );
+
 }
