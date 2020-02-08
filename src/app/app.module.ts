b4c57885a4fb6e5d19cef6cb98b1f2e82a164003
Stefano Notaro <stefano.notaro@gmail.com>
2020-02-08 18:57:43 -0300

    Navbar generalization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-// import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http'; // Http client import
 
 // Routes
 import { APP_ROUTES } from './app.routes';
--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  rutas: string[] = ['Home', 'Link'];
+
   constructor() { }
 
   ngOnInit() {
