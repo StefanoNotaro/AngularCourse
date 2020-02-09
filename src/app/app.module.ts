06142e8ce682e9c21c5fd42be0eaaeb27424ae0d
Stefano Notaro <stefano.notaro@gmail.com>
2020-02-09 02:45:10 -0300

    Starting section 22

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ScrollingModule } from "@angular/cdk/scrolling";
 // import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http'; // Http client import
 
 // Routes
 // import { APP_ROUTES } from './app.routes';
 
 import { AppComponent } from './app.component';
+import { VirtualComponent } from './components/virtual/virtual.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VirtualComponent
   ],
   imports: [
     BrowserModule,
+    ScrollingModule,
     // APP_ROUTES,
   ],
   providers: [],
